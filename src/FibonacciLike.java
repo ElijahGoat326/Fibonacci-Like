74ad55865c941d7464ec9cbcaa1febbccd88eb32
ElijahGoat326 <elijahlopez326@gmail.com>
2023-04-26 08:40:06 -0400

    started sequence

---
+++ src/FibonacciLike.java
@@ -0,0 +1,18 @@
+public class FibonacciLike {
+       int NumbersInSequence;
+       int number1;
+       int number2;
+
+
+       public FibonacciLike(int numbersInSequence, int number1, int number2);{
+              this.NumbersInSequence
+              this.number1
+              this.number2
+       }
+
+
+
+
+
+
+}
