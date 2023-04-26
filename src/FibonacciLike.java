61bc9e70bb00734950e4ec0d989f3740dc1206cb
ElijahGoat326 <elijahlopez326@gmail.com>
2023-04-26 09:06:09 -0400

    fixed some issues

--- src/FibonacciLike.java
+++ src/FibonacciLike.java
@@ -4,15 +4,11 @@ public class FibonacciLike {
        int number2;
 
 
-       public FibonacciLike(int numbersInSequence, int number1, int number2);{
-              this.NumbersInSequence
-              this.number1
-              this.number2
+
+
+
+
+       public FibonacciLike() {
+
        }
-
-
-
-
-
-
 }
