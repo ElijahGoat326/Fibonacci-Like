5608aca3bdcba02ced647c02379cabca821734c7
ElijahGoat326 <elijahlopez326@gmail.com>
2023-04-27 10:30:08 -0400

    One step away from finishing

--- src/FibonacciLike.java
+++ src/FibonacciLike.java
@@ -1,14 +1,30 @@
-public class FibonacciLike {
+public class FibonacciLike implements FibonacciFace {
        int NumbersInSequence;
        int number1;
        int number2;
 
 
+       public FibonacciLike(int numbersInSequence, int number1, int number2) {
+              NumbersInSequence = numbersInSequence;
+              this.number1 = number1;
+              this.number2 = number2;
+       }
 
 
 
+       public void run() {
+              // (5, 3, 7)
+              // 3, 7, 10, 17, 27
+              System.out.println(number1);
+              System.out.println(number2);
+              int nextNumber = number1 + number2;
+              System.out.println(number1 + number2);
+              for (int i = 3 ; i < NumbersInSequence; i = i + 1) {
 
-       public FibonacciLike() {
-
+                     nextNumber = number2 + nextNumber;
+                     number1 = number2;
+                     number2 = nextNumber ;
+                     System.out.println(nextNumber);
+              }
        }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Let's run Fibonacci");
-        FibonacciLike Fibonacci1 = new FibonacciLike();
+        FibonacciLike Fibonacci1 = new FibonacciLike(5, 3, 7);
+        Fibonacci1.run();
 
+        FibonacciLike Fibonacci2 = new FibonacciLike(8, -1, 4);
+       // Fibonacci2.run();
 
 
     }
